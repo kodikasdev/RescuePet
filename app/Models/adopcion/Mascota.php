50c58c5e8187e6090177a6b07c832ecdb4b62c8a
Miguel Ernesto Uicab Canabal <mortmr9@gmail.com>
2021-09-06 17:01:28 -0500

    PETS V1: Instalación delñ framework, creacion de los modelos(Comportamiento, Enfermedade, entrega, Localizaciones y Mascota) y sus respectivas migraciones

---
+++ app/Models/adopcion/Mascota.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\adopcion;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Mascota extends Model
+{
+    use HasFactory;
+}
