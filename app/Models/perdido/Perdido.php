e4469eb5141cf916fb44d5f20114dd26dea63edc
willian cg <willianeduardocg13@gmail.com>
2021-11-06 19:24:39 -0600

    cambiando la relacion

--- app/Models/perdido/Perdido.php
+++ app/Models/perdido/Perdido.php
@@ -25,7 +25,7 @@ class Perdido extends Model
 
     public function localizacione()
     {
-        return $this->belongsTo(Localizacione::class,'mascota_id');
+        return $this->hasOne(Localizacione::class,'mascota_id');
     }
 
 }
