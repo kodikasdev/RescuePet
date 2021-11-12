27377d18bc2581f48bf476431afbc63940d5d5a0
willian cg <willianeduardocg13@gmail.com>
2021-11-12 02:18:38 -0600

    ruta post para actualizar

--- routes/admin.php
+++ routes/admin.php
@@ -19,5 +19,6 @@ Route::post('adopcion/{mascota}/files', [adopcionController::class, 'files'])->n
 Route::get('perdidos', PerdidosIndex::class)->name('perdido.index');
 Route::get('perdidos/create', PerdidosCreate::class)->name('perdido.create');
 Route::get('perdidos/{mascota}/edit', PerdidosEdit::class)->name('perdido.edit');
+Route::post('perdidos/{mascota}/edit', PerdidosEdit::class,)->name('perdido.edit');
 Route::post('perdidos/{mascota}/files', [perdidosController::class, 'files'])->name('perdido.files');
 
