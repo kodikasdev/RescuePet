96f742ed4a939dbfd09e93908da721c9695997f9
Miguel Ernesto Uicab Canabal <mortmr9@gmail.com>
2021-11-13 13:26:30 -0600

    Agregar assign role

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -24,6 +24,6 @@ class UserSeeder extends Seeder
             'name'=>'will',
             'email'=>'correo@correo.com',
             'password'=>bcrypt('12345678')
-        ]);
+        ])->assignRole('Administrador');;
     }
 }
